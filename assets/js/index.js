bff14dde79ae81b83e9fb2af7dac55e8c7c71849
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-03-11 12:38:42 -0500

    se cambio versionado a 1.0.4

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // import { initialApp } from './app.js';
 // import { procesA, procesB, procesC } from './home.js';
 
-const version = '?v=1.0.3';
+const version = '?v=1.0.4';
 
 const { initialApp } = await import(`./app.js${version}`);
 const { procesA, procesB, procesC } = await import(`./home.js${version}`);
