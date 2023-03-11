81dc55003c3445c3ab6c2e9340395cf7a0286f65
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-03-11 12:35:22 -0500

    se agrego metodo de versionado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
-import { initialApp } from './app.js';
-import { procesA, procesB, procesC } from './home.js';
+// import { initialApp } from './app.js';
+// import { procesA, procesB, procesC } from './home.js';
+
+const version = '?v=1.0.3';
+
+const { initialApp } = await import(`./app.js${version}`);
+const { procesA, procesB, procesC } = await import(`./home.js${version}`);
+
 initialApp();
 
 procesA("parrafo-1");
